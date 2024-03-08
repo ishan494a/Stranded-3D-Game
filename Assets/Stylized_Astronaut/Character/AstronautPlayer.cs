b3bcd4cd239d03700b1e0a66338755f25d7216cd
ishanuwo <ishanuwo@github.com>
2024-03-07 22:14:21 -0500

    added jump functions

--- Assets/Stylized_Astronaut/Character/AstronautPlayer.cs
+++ Assets/Stylized_Astronaut/Character/AstronautPlayer.cs
@@ -13,24 +13,51 @@ namespace AstronautPlayer
 		public float turnSpeed = 400.0f;
 		private Vector3 moveDirection = Vector3.zero;
 		public float gravity = 20.0f;
+        private float jumpSpeed = 8;
 
-		void Start () {
+        private bool isJumping;
+        private bool isFalling;
+
+        void Start () {
 			controller = GetComponent <CharacterController>();
 			anim = gameObject.GetComponentInChildren<Animator>();
 		}
 
 		void Update (){
-			if (Input.GetKey ("w")) {
+            anim.SetBool("isJumping", false);
+            if (Input.GetKey ("w")) {
 				anim.SetInteger ("AnimationPar", 1);
 			}  else {
 				anim.SetInteger ("AnimationPar", 0);
 			}
+            if (controller.isGrounded)
+            {
+                moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+				if (Input.GetKey("space"))
+				{
+                    moveDirection.y = jumpSpeed;
+                    isJumping = true;
+					anim.SetBool("isJumping", true);
+				}
+                else
+                {
+                    isJumping = false;
+                    anim.SetBool("isJumping", false);
+                }
 
-			if(controller.isGrounded){
-				moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
-			}
-
-			float turn = Input.GetAxis("Horizontal");
+            }
+            else
+            {
+                if (isJumping)
+                {
+                    anim.SetBool("isFalling", true);
+                } else
+                {
+                    anim.SetBool("isFalling", false);
+                }
+            }
+			anim.SetBool("grounded", true);
+            float turn = Input.GetAxis("Horizontal");
 			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
 			controller.Move(moveDirection * Time.deltaTime);
 			moveDirection.y -= gravity * Time.deltaTime;
